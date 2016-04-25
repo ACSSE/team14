defa3c3c2a9105a86d7813dbb89b79519525e9f6
rommel234 <rommelkms@outlook.com>
2016-04-25 11:07:51 +0300

    Added files via upload
    The registration page for new users that are not registered yet.

---
+++ registration.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Handy_Man_2._0
+{
+    public partial class registration : PhoneApplicationPage
+    {
+        public registration()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/login.xaml", UriKind.Relative));
+        }
+    }
+}
