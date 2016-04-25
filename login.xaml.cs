b8a9098d1673ba55c1918abd6b49595fa9884a76
rommel234 <rommelkms@outlook.com>
2016-04-25 11:02:32 +0300

    Added files via upload
    The login page for the user input the credentials in order to login on the system

---
+++ login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Handy_Man_2._0
+{
+    public partial class login : PhoneApplicationPage
+    {
+        clientsDetails details = new clientsDetails
+        {
+
+        };
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/profile.xaml", UriKind.Relative));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/registration.xaml", UriKind.Relative));
+        }
+    }
+}
