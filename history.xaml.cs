b47696bdc37645d74ebf9d88c435c8b58b451523
rommel234 <rommelkms@outlook.com>
2016-04-25 11:29:35 +0300

    Added files via upload
    this page uses information of different jobs done by the Handy Man and displays clients name, comment and location. Just to help him to see if his work is being appreciate by the clients that he or her dealt with.

---
+++ history.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Handy_Man_2._0
+{
+    public partial class history : PhoneApplicationPage
+    {
+        public history()
+        {
+            InitializeComponent();
+        }
+    }
+}
