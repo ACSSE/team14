eee5d85c13dc8b3d0daba24fff92226c883c62d9
AnnaKN <anna.kapinga@yahoo.com>
2016-09-13 03:22:02 +0200

    Find me feature working

--- Project1/findme.aspx.vb
+++ Project1/findme.aspx.vb
@@ -7,16 +7,25 @@ Public Class findme
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+       
+
+    End Sub
+
+    Protected Sub btnReg_Click(sender As Object, e As EventArgs) Handles btnReg.ServerClick
+
         Dim connection As SqlConnection = New SqlConnection("Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\HandymanDatabase.mdf;Integrated Security=True")
         connection.Open()
         Dim query As String = "INSERT INTO Location (Town, Surburb) VALUES (@town, @suburb);"
         Dim command As SqlCommand = New SqlCommand(query, connection)
 
-        command.Parameters.AddWithValue("@town", txtTown)
-        command.Parameters.AddWithValue("@suburb", txtSuburb)
+        command.Parameters.AddWithValue("@town", txtTown.Text())
+        command.Parameters.AddWithValue("@suburb", txtSuburb.Text())
 
         Dim reader As SqlDataReader = command.ExecuteReader()
+
         connection.Close()
+
+        Response.Redirect("RegisterClient.aspx")
 
     End Sub
 
