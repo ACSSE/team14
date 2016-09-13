5a625bf5b5a7ed2cf5d6391a342f412ac5dce077
JohannesKM <201302051@student.uj.ac.za>
2016-09-13 03:14:51 +0200

    Displays number of users who joined for every up to the latest

--- Project1/AdminStats.aspx.vb
+++ Project1/AdminStats.aspx.vb
@@ -26,6 +26,46 @@ Public Class AdminStats1
             Dim month As Integer = workers(i).getDate().Month
             months(month)(1) += 1
         Next i
+
+        Dim html As String = ""
+
+        Dim latestmonth As Integer = Date.Now.Month
+
+        For i As Integer = 1 To latestmonth
+            Select Case i
+                Case 12
+
+                Case 11
+
+                Case 10
+                    html &= "<p>October &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(10)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(10)(2)
+                Case 9
+                    html &= "September &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(9)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(9)(2) & "<p></p>"
+                Case 8
+                    html &= "<p>August &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(8)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(8)(2)
+                Case 7
+                    html &= "July &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(7)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(7)(2)
+                Case 6
+                    html &= "June &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(6)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(6)(2)
+                Case 5
+                    html &= "May &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(5)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(5)(2)
+                Case 4
+                    html &= "April &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(4)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(4)(2)
+                Case 3
+                    html &= "March &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(3)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(3)(2)
+                Case 2
+                    html &= "February &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(2)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(2)(2)
+                Case 1
+                    html &= "January &nbps&nbps&nbps&nbps&nbps&nbps&nbps " & months(1)(1) & "&nbps&nbps&nbps&nbps&nbps&nbps&nbps" & months(1)(2)
+
+            End Select
+        Next i
+
+
+       
+
+        VisiterStats.InnerHtml = html
+
     End Sub
 
     Private Function getClients()
