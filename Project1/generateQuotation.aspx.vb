12a40686d8c08b2c5c1d2de227ce8cb827c5f028
AnnaKN <anna.kapinga@yahoo.com>
2016-09-13 17:00:00 +0200

    deleted temp client profiles

--- Project1/generateQuotation.aspx.vb
+++ Project1/generateQuotation.aspx.vb
@@ -7,44 +7,29 @@
 
     Protected Sub btnReg_Click(sender As Object, e As EventArgs) Handles btnReg.ServerClick
 
-        'Dim description As String = ""
-        'Dim hours As String = ""
-        'Dim amount As String = ""
+        Dim description As String = ""
+        Dim hours As String = ""
+        Dim amount As String = ""
 
+            username = txtUsername.Text()
+            password = txtPassword.Text()
+            name = txtName.Text()
+            surname = txtSurname.Text()
+            email = txtEmail.Text()
+            numbers = txtMobile.Text()
+           
 
-        'Dim tempClient As Worker = New Worker(username, password)
+            MsgBox("RegisterWorker():btnReg- category = " & category)
 
-        'If tempClient.getUsername() = "" Then
-        '    username = txtUsername.Text()
-        '    password = txtPassword.Text()
-        '    name = txtName.Text()
-        '    surname = txtSurname.Text()
-        '    email = txtEmail.Text()
-        '    numbers = txtMobile.Text()
-        '    ' category  'categoriesList.SelectedValue() 'txtTitle.Text()
+            description = txtDescription.Text()
 
-        '    'To obtain string with all the selected categories
-        '    For i As Integer = 0 To lstCategory.Items.Count() - 1
-        '        If lstCategory.Items(i).Selected() Then 'list of selsected items
-        '            If category = "" Then
-        '                category = lstCategory.Items(i).Text() 'getting the slected item
-        '            Else
-        '                category &= " & " & lstCategory.Items(i).Text()
-        '            End If
-        '        End If
-        '    Next i
+            Dim worker As Worker = New Worker(username, password, name, surname, email, numbers, region, description, category, Nothing)
+            worker.saveUser()
+            Dim cUser As User = worker
+            Session("user") = cUser
+            'Session("UserName") = User
+            Response.Redirect("WorkerProfile.aspx")
 
-        '    MsgBox("RegisterWorker():btnReg- category = " & category)
-
-        '    description = txtDescription.Text()
-
-        '    Dim worker As Worker = New Worker(username, password, name, surname, email, numbers, region, description, category, Nothing)
-        '    worker.saveUser()
-        '    Dim cUser As User = worker
-        '    Session("user") = cUser
-        '    'Session("UserName") = User
-        '    Response.Redirect("WorkerProfile.aspx")
-        'End If
 
     End Sub
 
