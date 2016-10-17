784a273fd4410213e26414890939f8f6bf2f0af3
AnnaKN <anna.kapinga@yahoo.com>
2016-10-17 10:08:59 +0200

    Client stat page changed

--- Project1/generateQuotation.aspx.vb
+++ Project1/generateQuotation.aspx.vb
@@ -1,4 +1,6 @@
-﻿Public Class generateQuotation
+﻿Imports System.Data.SqlClient
+
+Public Class generateQuotation
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -9,24 +11,31 @@
 
 
         Dim quoteId As String = ""
-        Dim quoteDescription As String = ""
-        Dim hours As Integer = ""
-        Dim quoteAmount As Integer = ""
+        Dim quoteDescription As String = lblQuoteDescription.Text()
+        Dim hours As Integer = lblQuoteHours.Text()
+        Dim quoteAmount As Integer
         Dim worker As Worker = Session("user")
 
         Dim workerUsername As String = worker.getUsername() 'worker who is writting the quotation
-       
-        quoteDescription = txtQuoteDescription.Text()
-        hours = txtQuoteHours.Text()
-        quoteAmount = txtQuoteAmount.Text
-
-           
 
 
-        Dim quotation As Quotation = New Quotation(quoteId, quoteDescription, hours, quoteAmount, workerUsername)
-        quotation.savequoteDescription()
 
-        Response.Redirect("WorkerProfile.aspx")
+
+        Dim connection As SqlConnection = New SqlConnection(ValidationClass.CONNECTIONSTRING)
+        Dim query As String = "INSERT INTO Quotation (QuoteId, quoteDescription, quoteHours, quoteAmount, Worker) Values (@ID, @quoteDescription, @quoteHours, @quoteAmount, @worker)"
+        connection.Open()
+
+        Dim command As SqlCommand = New SqlCommand(query, connection)
+        command.Parameters.AddWithValue("@ID", quoteId)
+        command.Parameters.AddWithValue("@quoteDescription", quoteDescription)
+        command.Parameters.AddWithValue("@quoteHours", quoteHours)
+        command.Parameters.AddWithValue("@quoteAmount", quoteAmount)
+        command.Parameters.AddWithValue("@worker", worker)
+
+        Dim reader As SqlDataReader = command.ExecuteReader()
+        connection.Close()
+
+        'Response.Redirect("QuotationDisplay.aspx")
 
 
 
