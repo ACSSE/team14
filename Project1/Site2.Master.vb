39d7c07214537666851ba05f5eddbd4289e3886e
AnnaKN <anna.kapinga@yahoo.com>
2016-10-18 11:02:55 +0200

    logout label changed

--- Project1/Site2.Master.vb
+++ Project1/Site2.Master.vb
@@ -8,7 +8,7 @@ Public Class Site2
             userLog.Visible = True
 
             If TypeOf cUser Is Client Then
-                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome " & "<b style=""color:#01A185"">" & cUser.getUsername() & "</b> " & "<a style="" font-size:small;""  href=""logout.aspx"">(logout)</a></p>&nbsp;&nbsp;" & countResponses()
+                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome " & "<b style=""color:#01A185"">" & cUser.getUsername() & "</b> " & "<a style="" font-size:small;""  href=""logout.aspx"">(logout)</a>&nbsp;&nbsp;" & countResponses() & "</p>"
             Else
                 userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome " & "<b style=""color:#01A185"">" & cUser.getUsername() & "</b> " & "<a  href=""logout.aspx"" style="" font-size:small;"">(logout)</a></p>"
             End If
--- Project1/Site3.Master.vb
+++ Project1/Site3.Master.vb
@@ -8,9 +8,9 @@ Public Class Site3
             userLog.Visible = True
 
             If TypeOf cUser Is Client Then
-                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome</p> " & "<p style=""color:#01A185""><b>" & cUser.getUsername() & "</b></p> " & "<a style="" font-size:small;""  href=""logout.aspx"">(logout)</a>&nbsp;&nbsp;" & countResponses()
+                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome " & "<b style=""color:#01A185"">" & cUser.getUsername() & "</b> " & "<a style="" font-size:small;""  href=""logout.aspx"">(logout)</a>&nbsp;&nbsp;" & countResponses() & "</p>"
             Else
-                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome</p> " & "<p style=""color:#01A185""><b>" & cUser.getUsername() & "</b></p> " & "<p><a  href=""logout.aspx"">(logout)</a><p>"
+                userLog.InnerHtml = "<p style=""color:#FBCC33"">Welcome " & "<b style=""color:#01A185"">" & cUser.getUsername() & "</b> " & "<a  href=""logout.aspx"" style="" font-size:small;"">(logout)</a></p>"
             End If
         End If
     End Sub
