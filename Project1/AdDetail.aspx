3d71dfbbd8a0e541460b32c20b87d0c485ee8bf7
AnnaKN <anna.kapinga@yahoo.com>
2016-10-21 07:38:02 +0200

    Worker's stat page modified

--- Project1/AdDetail.aspx
+++ Project1/AdDetail.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site2.Master" CodeBehind="AdDetail.aspx.vb" Inherits="Project1.AdDetail" %>
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site3.Master" CodeBehind="AdDetail.aspx.vb" Inherits="Project1.AdDetail" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
--- Project1/workerStat.aspx.vb
+++ Project1/workerStat.aspx.vb
@@ -3,6 +3,57 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        Dim currentDate As Date = Date.Now
+        Dim currentHour As Integer = TimeOfDay.Hour
+        Dim currentMinutes As Integer = TimeOfDay.Minute
+
+        Dim type As String = Request.QueryString("type")
+
+
+        If type = "client" Then
+            Dim username As String = Request.QueryString("username")
+
+            Worker = New Worker(username)
+
+            divrating.InnerHtml = "<h4>Rating</h4>" & ValidationClass.getRateImage(Worker.getRating())
+
+            JobTitle.InnerText = Worker.getCategory() 'setting the correct heading category
+            lblRegion.InnerText = Worker.getRegion()
+            lblName.InnerText = Worker.getName()
+            lblSurname.InnerHtml = Worker.getSurname()
+            lblNumber.InnerText = Worker.getNumbers()
+            lblEmail.InnerText = Worker.getEmail()
+            lblRegion.InnerText = Worker.getRegion()
+            personalAd.InnerHtml = "<a href=""PostAdClient.aspx?adType=" & Worker.getUsername() & """>" & "Post an ad to " & Worker.getUsername() & "</a>"
+            getHistory() ' to display all the previous work done by the worker
+
+        Else
+            Dim c As User = Session("user")
+            Worker = c
+
+
+
+            lblRegion.Visible = True
+            lblName.Visible = True
+            lblSurname.Visible = True
+            lblNumber.Visible = True
+            lblEmail.Visible = True
+
+            divrating.InnerHtml = "<h3>Rating</h3>" & ValidationClass.getRateImage(Worker.getRating())
+
+            JobTitle.InnerText = Worker.getCategory() 'setting the correct heading category
+            lblRegion.InnerText = Worker.getRegion()
+            lblName.InnerText = Worker.getName()
+            lblSurname.InnerHtml = Worker.getSurname()
+            lblNumber.InnerText = Worker.getNumbers()
+            lblEmail.InnerText = Worker.getEmail()
+
+            personalJobs.InnerHtml = displayPersonalJobs()
+            myJobs.InnerHtml = displayJobs()
+            JobNots.InnerHtml = displayJobs(Worker.getCategory())
+            penJobs.InnerHtml = displayPendingJobs()
+            getHistory() ' to display all the previous work done by the worker
+        End If
 
 
     End Sub
